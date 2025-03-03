05a3ba28cf5229278b8a6c5a3d72de71dba6e058
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2025-03-03 14:44:54 +0530

    Added sort colors from leetcode
    sorting an array having 0s,1s and 2s without sort method

--- src/arraysPrac/Prac1.java
+++ src/arraysPrac/Prac1.java
@@ -12,10 +12,35 @@ public class Prac1 {
 
         //Best
     }
+    //
+    public static int[] sortColors(int[] nums) {
+        int low=0,mid=0,high=nums.length-1,i=0;
+        while(mid <= high){
+            if(nums[i]==0){
+                int temp =  nums[low];
+                nums[low] = nums[i];
+                nums[i] = temp;
+                ++low;
+                ++mid;
+                ++i;
+            }else if(nums[i]==1){
+                ++mid;
+                ++i;
+            }else{
+                int temp =  nums[high];
+                nums[high] = nums[i];
+                nums[i] = temp;
+                --high;
+            }
+        }
+        return nums;
+    }
 
     public static void main(String[] args) {
         /*Left Rotate the array by one place*/
-        int arr[] ={1,2,3,4,5};//54321
+
+        System.out.println(Arrays.toString(sortColors(new int[]{2,0,2,1,1,0})));
+        /*int arr[] ={1,2,3,4,5};//54321
         int rotations=3;
         int actualToRotate = rotations%arr.length;
 //        start->rotations
@@ -25,11 +50,11 @@ public class Prac1 {
             if((i+actualToRotate)<arr.length)
                 start[i] = arr[i+actualToRotate];
         }
-        for()
+//        for()
         Arrays.stream(start).forEach(System.out::print);
         System.out.println();
         Arrays.stream(slide).forEach(System.out::print);
-        //for(int i = 0; i<actualToRotate)
+        //for(int i = 0; i<actualToRotate)*/
 
 
     }
