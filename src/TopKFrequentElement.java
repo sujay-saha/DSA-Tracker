370f9461c7082daf6a3cee5ce2a60ab7346ec871
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2025-03-28 22:00:55 +0530

    Added BucketSort to get the count of elements in O(n) timne complexity
    +Taking the count as index added the keys to track counts easily

--- src/TopKFrequentElement.java
+++ src/TopKFrequentElement.java
@@ -21,7 +21,58 @@ public class TopKFrequentElement {
         return res;
     }
 
+
+    public static int[] topKFrequentPriorityQueue(int[] nums, int k) {
+        int []res=new int[k];
+        Map<Integer,Integer> elementCount = new HashMap<>();
+        for(int num:nums){
+            elementCount.put(num, elementCount.getOrDefault(num,0)+1);
+        }
+
+        Queue<Integer> minHeap = new PriorityQueue<>((a,b)->elementCount.get(a)-elementCount.get(b));
+
+        for(int key:elementCount.keySet()){
+            minHeap.add(key);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        while(k-->0){
+            res[k]=minHeap.remove();
+        }
+
+        return res;
+    }
+
+    public static int[] topKFrequentBucketSort(int[] nums, int k) {
+        //The indexes act as frequency for Bucket Sort
+        int []res=new int[k];
+        Map<Integer,Integer> elementCount = new HashMap<>();
+        for(int num:nums){
+            elementCount.put(num, elementCount.getOrDefault(num,0)+1);
+        }
+
+        List<Integer>[] buckets = new ArrayList[nums.length+1];
+        for(int i = 0 ; i<nums.length+1;i++){
+            buckets[i] = new ArrayList<>();
+        }
+        for(Map.Entry<Integer,Integer> entry : elementCount.entrySet()){
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+
+        for (int index = buckets.length-1;index>0 && k>0;index--){
+            if(!buckets[index].isEmpty()){
+                for(Integer val:buckets[index]) {
+                    res[res.length - k--] = val;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Arrays.stream(topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)).forEach(x->System.out.print(x+","));
+        System.out.println("---");System.out.println("PriorityQueue:::");Arrays.stream(topKFrequentPriorityQueue(new int[]{1, 1, 1, 2, 2, 3}, 2)).forEach(x->System.out.print(x+","));
+        System.out.println("---");System.out.println("BucketSort:::");Arrays.stream(topKFrequentBucketSort(new int[]{1, 1, 1, 2, 2, 3}, 2)).forEach(x->System.out.print(x+","));
     }
 }
