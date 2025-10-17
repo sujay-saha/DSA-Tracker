a2cbc451cc3ec374bc8c7300cc0d525918f284c6
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2025-10-17 13:38:26 +0530

    Created StockSpanner
    +Brute

---
+++ src/arraysPrac/StockSpanner.java
@@ -0,0 +1,34 @@
+package arraysPrac;
+
+public class StockSpanner {
+
+    class StockSpanner {
+        int maxPrice= Integer.MIN_VALUE;
+
+        List<Integer> stocksTillToday;
+        public StockSpanner() {
+            stocksTillToday = new ArrayList<>();
+
+        }
+
+        public int next(int price) {
+            stocksTillToday.add(price);
+            int n = stocksTillToday.size();
+            int count=0;
+            for(int i=n-1;i>=0;i--){
+                int stockPrev = stocksTillToday.get(i);
+                if(price>=stockPrev){
+                    ++count;
+                    continue;
+                }else{break;}
+            }
+            return count;
+        }
+    }
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+}
