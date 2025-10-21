ce1e9b8bed0ccc3d2a13791cd8dcc7c2b9e856a2
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2025-10-21 13:15:08 +0530

    Solved CouseSchedule I using DFS

--- src/graph/GraphPrac4.java
+++ src/graph/GraphPrac4.java
@@ -1,5 +1,8 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphPrac4 {
 
     public static void main(String[] args) {
@@ -7,6 +10,44 @@ public class GraphPrac4 {
     }
 
     //Course Schedule I
+    public boolean canFinishUsingDFS(int numCourses, int[][] prerequisites) {
+        int n=prerequisites.length;
+        boolean vis[]= new boolean[numCourses];
+        List<List<Integer>> adj= new ArrayList<>();
+        for(int i =0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                boolean inRecursion[]= new boolean[numCourses];
+                if(!dfs(i,vis,adj,inRecursion)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int node, boolean vis[],List<List<Integer>> adj,boolean inRecursion[] ){
+        vis[node]=true;
+        inRecursion[node]=true;
+        for(int neighbour:adj.get(node)){
+            if(inRecursion[neighbour]){
+                return false;
+            }
+            if(!vis[neighbour]){
+                if(!dfs(neighbour,vis,adj,inRecursion)){
+                    return false;
+                }
+            }
+        }
+        inRecursion[node]=false;
+
+        return true;
+    }
 
 
 
