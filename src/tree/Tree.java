3e7775150708bbd8ced6a2efcedfa58a345a6347
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2026-09-08 00:07:55 +0530

    Add levelOrder method to Tree class
    Implement level order traversal for a binary tree.

---
+++ src/tree/Tree.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Tree {
+    public static void main(String[] args) {
+      
+    }
+//Level Order Traversal
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q= new LinkedList<TreeNode>();
+        List<List<Integer>> resultList = new LinkedList<List<Integer>>();
+        if(root ==null){
+            return resultList;
+        }
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> sub = new LinkedList<Integer>();
+            for(int i =0;i<size;i++){
+                if(q.peek().left!=null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.offer(q.peek().right);
+                }
+                sub.add(q.poll().val);
+            }
+            resultList.add(sub);
+        }
+        return resultList;
+    }
+}
