e2629a49c7bb5512e0ffe09381176e78f500f28d
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2026-09-08 10:35:00 +0530

    Implement level order traversal with leaf node sum
    Added levelOrder2 method to compute leaf node sum and return level order traversal.

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,5 +1,19 @@
 import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Tree {
     public static void main(String[] args) {
       
@@ -27,6 +41,36 @@ public class Tree {
             }
             resultList.add(sub);
         }
+        return resultList;
+    }
+    //Leaf Node Sum- Thought of a question and solved it
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        Queue<TreeNode> q= new LinkedList<TreeNode>();
+        List<List<Integer>> resultList = new LinkedList<List<Integer>>();
+        if(root ==null){
+            return resultList;
+        }
+        q.offer(root);
+        int sum=0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> sub = new LinkedList<Integer>();
+            for(int i =0;i<size;i++){
+                if(q.peek().left!=null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.offer(q.peek().right);
+                }
+                if(q.peek().left==null && q.peek().left==null)
+                {
+                    sum+=q.peek().val;
+                }
+                sub.add(q.poll().val);
+            }
+            resultList.add(sub);
+        }
+        System.out.println(sum);
         return resultList;
     }
 }
