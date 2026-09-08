2ff2f53529f7ed205dc0bc3b7f9e7bb4dbd3a66f
Sujay Saha <60770842+sujay-saha@users.noreply.github.com>
2026-09-08 12:04:18 +0530

    Implement iterative preorder traversal in Tree.java
    Added an iterative preorder traversal method to Tree.java.

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -73,4 +73,27 @@ public class Tree {
         System.out.println(sum);
         return resultList;
     }
+
+    //iterative preorder traversal
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> result = new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            result.add(root.val);
+            if(root.right!=null){
+                stack.push(root.right);
+            }
+            if(root.left!=null){
+                stack.push(root.left);
+            }
+        }
+        return result;
+    }
 }
